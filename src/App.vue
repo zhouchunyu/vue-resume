0db173d9525538cf180342b8c927f113110e5aec
zhouchunyu <zhouchunyu20xx@163.com>
2018-08-02 19:30:57 +0800

    初始页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="app">
-    <Editor/>
-    <Preview/>
-    <Topbar/>
+    <div class="topbar">
+      <Topbar/>
+    </div>
+    <main>
+      <Editor class="editor"/>
+      <Preview class="preview"/>
+    </main>
   </div>
 </template>
 
@@ -24,5 +28,27 @@ export default {
 <style>
 #app {
   font-family: -apple-system, "Noto Sans", "Helvetica Neue", Helvetica, "Nimbus Sans L", Arial, "Liberation Sans", "PingFang SC", "Hiragino Sans GB", "Noto Sans CJK SC", "Source Han Sans SC", "Source Han Sans CN", "Microsoft YaHei", "Wenquanyi Micro Hei", "WenQuanYi Zen Hei", "ST Heiti", SimHei, "WenQuanYi Zen Hei Sharp", sans-serif;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+main {
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+}
+
+.topbar{
+  height: 10em;
+  background-color: red;
+}
+
+.editor{
+  width: 20em;
+  background-color: green;
+}
+.preview{
+  flex: 1;
+  background-color: blue;
 }
 </style>
